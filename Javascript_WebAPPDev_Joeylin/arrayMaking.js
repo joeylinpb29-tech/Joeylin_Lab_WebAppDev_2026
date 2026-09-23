d33ce1e995a36f943f17433a689cbaedbb2d3acb
Joey <joeylinpb29@gmail.com>
2026-09-23 12:47:15 +0700

    fix submodules

---
+++ Javascript_WebAPPDev_Joeylin/arrayMaking.js
@@ -0,0 +1,11 @@
+const colours = ['red', 'green', 'blue', 'yellow', 'purple'];
+
+colours[0]; //"red"
+
+colours.length;
+
+// Important array methods:
+//push(value) - adds value to the end of the array
+colours.push('orange'); // adds 'orange' to the end of the array
+
+//pop()
